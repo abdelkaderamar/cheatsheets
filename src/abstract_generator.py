d87420b107f28ac9c999c7132e58973fff80ce68
Abdelkader Amar <abdelkader.amar@gmail.com>
2022-09-26 12:04:15 +0200

    Adding columnbreak/newpage to yaml format

--- src/abstract_generator.py
+++ src/abstract_generator.py
@@ -1,15 +1,16 @@
 from re import template
+from rich.console import Console
 
 
 class abstract_generator:
 
     LINE_NUMBER_PER_COLUMN=82
 
-    def __init__(self, line_per_col=LINE_NUMBER_PER_COLUMN):
-        self.line_per_col=line_per_col
-        self.line_count=0
-        self.column_count=1
-        with open('template_cheatsheet.tex', 'r') as template_file:
+    def __init__(self, tex_template='template_cheatsheet.tex'):
+        self.console = Console()
+        self.console.print("Initialializing the generator")
+
+        with open(tex_template, 'r') as template_file:
             self.template_content=template_file.read()
         self.start=self.template_content.index('%%BEGIN_ITEM%%')
         print(self.template_content.index('%%END_ITEM%%'))
@@ -21,24 +22,15 @@ class abstract_generator:
     def do_generate(self, items, main_title, latex_file):
         # Add items & columnbreaks
         for item in items:
-            if self.column_count == 4:
-                self.column_count = 1
-                self.line_count = 0
-                self.generated_content += "\\end{multicols}\n"
-                self.generated_content += "\\newpage\n"
-                self.generated_content += "\\vspace*{1cm}\n"
-                self.generated_content += "\\begin{multicols}{3}"
-            print(self.generated_content)
+            self.console.print(item)
+            if item == 'linebreak':
+                self.generated_content += self.add_columnbreak()
+                continue
+            if item == 'newpage':
+                self.generated_content += self.add_newpage()
+                continue
             self.generated_content+=self.generate_item(item, self.templat_item)
-
-        self.console.print(f"current column_count = {self.column_count}")
-        for i in range(self.column_count,3):
-            self.console.print("Adding a column break")
-            self.generated_content += self.add_columnbreak()
-            self.generated_content += '\\,'
-        
         self.generated_content+=self.template_content[self.end:]
-        #self.console.print(self.generated_content)
         
         title=main_title
         self.generated_content=self.generated_content.replace('%%MAIN_TITLE%%', title)
@@ -51,15 +43,6 @@ class abstract_generator:
         item_latex=template_item.replace('%%BEGIN_ITEM%%','').replace('%%END_ITEM%%', '')
         item_latex = item_latex.replace('%%ITEM_TITLE%%', title)
         item_latex = item_latex.replace('%%ITEM_CONTENT%%', content)
-        item_lines_count=item_latex.count('\n')
-        print(f'line count={item_lines_count}')
-        if (self.line_count + item_lines_count > self.line_per_col):
-            self.line_count = 0
-            self.column_count = self.column_count + 1
-            if self.column_count != 4:
-                item_latex = item_latex + self.add_columnbreak()
-        else:
-            self.line_count += item_lines_count
         return item_latex
 
 
@@ -68,3 +51,10 @@ class abstract_generator:
         columnbreak += '\\columnbreak\n'
         columnbreak += '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n'
         return columnbreak
+    
+    def add_newpage(self):
+        newpage = "\\end{multicols}\n"
+        newpage += "\\newpage\n"
+        newpage += "\\vspace*{1cm}\n"
+        newpage += "\\begin{multicols}{3}"
+        return newpage
