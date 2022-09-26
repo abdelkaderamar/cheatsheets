3d9584a1b5cb9d24f8b239d664feb209489ce7d2
Abdelkader Amar <abdelkader.amar@gmail.com>
2022-09-26 15:41:44 +0200

    Update colourful template generator

--- src/md_to_latex_colourful.py
+++ src/md_to_latex_colourful.py
@@ -1,7 +1,7 @@
 import sys
-import marko
 from rich import inspect
 from rich.console import Console
+import argparse
 
 from abstract_generator import abstract_generator
 
@@ -9,7 +9,10 @@ from abstract_generator import abstract_generator
 
 class md_to_latex_generator(abstract_generator):
 
-    def __init__(self, filename: str, tex_template = 'cheatsheet_template_colourful.tex'):
+    COLOURFUL_TEMPLATE_FILE = 'colourful-template.tex'
+    COLOURFUL_UPDATE_TEMPLATE_FILE = 'colourful-updated-template.tex'
+
+    def __init__(self, filename: str, tex_template = COLOURFUL_TEMPLATE_FILE):
         super().__init__(tex_template=tex_template)
         self.console = Console()
 
@@ -54,22 +57,26 @@ class md_to_latex_generator(abstract_generator):
                     data['items'].append({'item': item})
         items=data['items']
         print(f"{len(items)} items")
-        latex_file=filename[0:filename.lower().rfind('.md')] + '-colourful-md.tex'
+        suffix='-' + tex_template.replace('-template', '')
+        latex_file=filename[0:filename.lower().rfind('.md')] + suffix
         self.do_generate(data['items'], main_title, latex_file)
 
 
 
 def main():
-    if len(sys.argv) < 2:
-        print(f"Syntax error.")
-        print("Usage: gen_cs <md file> [colourful_tex_template]")
-        sys.exit(1)
-    
-    if len(sys.argv) >= 3:
-        generator=md_to_latex_generator(sys.argv[1], tex_template=sys.argv[2])
-    else:
-        generator=md_to_latex_generator(sys.argv[1])
-    # generator.generate()
+    parser = argparse.ArgumentParser(description='Process a markdown file and generate tex file using Colourful (or Colourful Update) template')
+    parser.add_argument('--colourful', '-c', default=True, action='store_true', dest='colourful', help='Use Colourful template (default)')
+    parser.add_argument('--colourful-upd', '-u', action='store_true', dest='colourful_updated', help='Use Colourful Updated template ')
+    parser.add_argument('markdown_file', help='Markdown file to parse')
+    args = parser.parse_args()
+    # print(args.colourful)
+    # print(args.colourful_updated)
+    # print(args.markdown_file)
+
+    if args.colourful_updated:
+        generator=md_to_latex_generator(args.markdown_file, md_to_latex_generator.COLOURFUL_UPDATE_TEMPLATE_FILE)
+    elif args.colourful:
+        generator=md_to_latex_generator(args.markdown_file)
 
 if __name__ == '__main__':
     main()
